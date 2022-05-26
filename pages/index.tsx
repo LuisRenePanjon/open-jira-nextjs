82c6eb329d771300f54da06368cbf10b5eb9e001
Luis René Panjón <luchorene97@gmail.com>
2022-05-25 19:23:15 -0500

    Designe index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { MainLayout } from '../components/layouts';
 
 const HomePage: NextPage = () => {
-    return <h1>Init Projects</h1>;
+    return <MainLayout title='OpenJira - Home'>
+        <Typography variant="h1">Hello World!</Typography>
+    </MainLayout>;
 };
 
 export default HomePage;
