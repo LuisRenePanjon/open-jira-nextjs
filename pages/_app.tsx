837dcaf5d30e301e93b26ca499c8882a33c25a31
Luis René Panjón <luchorene97@gmail.com>
2022-05-25 19:24:40 -0500

    Wrapp App in a ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { darkTheme, lightTheme } from '../themes';
 
-const basicTheme = createTheme({
-    palette: {
-        primary: {
-            main: '#00bcd4',
-        },
-        secondary: {
-            main: '#f50057',
-        },
-        error: {
-            main: '#f50057',
-        },
-        mode: 'light',
-    },
-    typography: {
-        fontFamily: 'Roboto',
-        fontSize: 14,
-        fontWeightLight: 300,
-        fontWeightRegular: 400,
-        fontWeightMedium: 500,
-        fontWeightBold: 700,
-    },
-    
-});
+
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ThemeProvider theme={basicTheme}>
+        <ThemeProvider theme={lightTheme}>
           <CssBaseline />
             <Component {...pageProps} />
         </ThemeProvider>
